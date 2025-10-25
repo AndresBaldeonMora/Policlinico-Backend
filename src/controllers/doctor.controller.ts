5b39bc3b5dd9cc8dbcf35522baec7b3e8c427d18
AndresBaldeonMora <202121057@urp.edu.pe>
2025-10-25 02:21:04 -0500

    .

--- src/controllers/doctor.controller.ts
+++ src/controllers/doctor.controller.ts
@@ -67,8 +67,7 @@ export const obtenerHorariosDisponibles = async (req: Request, res: Response) =>
     // Horarios del consultorio (8:00 AM - 5:00 PM, cada 30 min)
     const horariosBase = [
       "08:00", "08:30", "09:00", "09:30", "10:00", "10:30", "11:00", "11:30",
-      "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30",
-      "16:00", "16:30", "17:00"
+      "12:00"
     ];
 
     // Buscar citas ya agendadas para ese doctor en esa fecha
